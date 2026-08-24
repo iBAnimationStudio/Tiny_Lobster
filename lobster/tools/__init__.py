403c5c96ee650e5bbc2be83ae6ef3a81d177038c
bittu <mollasahib28@gmail.com>
2026-08-24 21:10:20 +0530

    Export WebTool in lobster/tools/__init__.py

--- lobster/tools/__init__.py
+++ lobster/tools/__init__.py
@@ -4,5 +4,6 @@ from lobster.tools.system import SystemInfoTool
 from lobster.tools.custom_tool_manager import CustomToolManager
 from lobster.tools.dynamic import DynamicTool
 from lobster.tools.memory_tool import MemoryTool
+from lobster.tools.web import WebTool
 
-__all__ = ["TerminalTool", "FileTool", "SystemInfoTool", "CustomToolManager", "DynamicTool", "MemoryTool"]
+__all__ = ["TerminalTool", "FileTool", "SystemInfoTool", "CustomToolManager", "DynamicTool", "MemoryTool", "WebTool"]
